5571ccb74205e72d09208d15d7a5f0504fedee3e
Katy <kma2@wellesley.edu>
2016-10-08 01:51:19 -0400

    added photography page

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ $('.read-more-content').addClass('hide');
 $('.read-less-content').addClass('hide');
 
 // read more toggle
-$('.read-more-toggle').on('click', function() {
+$('.read-more-button').on('click', function() {
   $(this).next('.read-more-content').toggleClass('hide');
   $(this).toggleClass('hide'); // hides link
 });
 
 // read less toggle
-$('.read-less-toggle').on('click', function() {
+$('.read-less-button').on('click', function() {
     $('.read-more-content').toggleClass('hide');
-    $('.read-more-toggle').toggleClass('hide'); // show read more toggle
+    $('.read-more-button').toggleClass('hide'); // show read more toggle
+});
+
+// back button to return to index
+$('.back-button').on('click', function() {
+    window.location = "../index.html";
+});
+
+// photography
+$("#project5").on('click', function(){
+     window.location = "views/photography.html";
 });
