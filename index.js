f4946ddb8a01fd03e77b329072466d71ce3561b9
Katy <kma2@wellesley.edu>
2017-11-16 00:17:44 -0500

    added lazy image loading

--- index.js
+++ index.js
@@ -26,4 +26,12 @@ $( document ).ready(function() {
   $('#microsoft-preview').click(function() {
     window.location = 'projects/microsoft.html';
   });
+
+  // lazy image loading
+  [].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = function() {
+      img.removeAttribute('data-src');
+    };
+  });
 });
